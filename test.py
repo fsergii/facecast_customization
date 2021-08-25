69d7bd83d537e75eef1d64091020bc1276cdab49
fsergii <fesenko.sergey@gmail.com>
2021-08-25 20:43:38 +0300

    Add files via upload

--- test.py
+++ test.py
@@ -1,1 +1,19 @@
-print("hello")
+from pyspark import SparkConf
+from pyspark.sql import SparkSession
+
+
+import os
+from pyspark import SparkConf
+from pyspark import SparkContext
+from pyspark.sql import SparkSession
+os.environ['PYSPARK_PYTHON'] = 'python3'
+spark_master = os.getenv('SPARK_MASTER', 'k8s://https://kubernetes.default.svc.cluster.local')
+spark = SparkSession.builder \
+    .master(spark_master) \
+    .config("spark.driver.maxResultSize", 0) \
+    .appName('cached_executor_test') \
+    .getOrCreate()
+ 
+sc = spark.sparkContext
+rdd = sc.parallelize([1,2,3,4,5,6,7,8,9,10]).cache()
+rdd.saveAsTextFile('spark_test.txt')
